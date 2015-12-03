6cc8c265f413e07b0dd06e29cbb80e425c4d5636
kotaro takahashi <kotaro.t@k4h4shi.com>
2015-12-03 20:22:59 +0900

    added brew.sh

---
+++ etc/init/osx/brew.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Stop script if errors occur
+trap 'echo Error: $:0$LINENO stopped; exit 1' ERR INT
+set -eu
+
+source "$DOTPATH"/etc/lib/core.sh
+source "$DOTPATH"/etc/lib/io.sh
+source "$DOTPATH"/etc/lib/util.sh
+
+# This script is only supported with OS X
+if ! is ox; then
+  log_fail "error: this script is only supported with os"
+  exit 1
+fi
+
+if has "brew"; then
+  log_pass "brew: already inatalled"
+  exit
+fi
+
+# The script dependent on ruby
+if ! has "ruby"; then
+  log_fail "error: require: ruby"
+  exit 1
+fi
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if has "brew"; then
+  brew doctor
+else
+  log_fail "error: brew: failed to insattl"
+  exit 1
+fi
+
+log_pass "brew: installed successfully"
